9bfbc38cd8b68a905a451ee9eb60722d8d21fe3f
Qaldak <qaldak@gmail.com>
2023-02-16 20:40:02 +0100

    check_container

--- src/check_container.py
+++ src/check_container.py
@@ -1,32 +1,25 @@
-from python_on_whales import docker, DockerException, ClientNotFoundError
+from python_on_whales import docker
 
 
 class Container:
     def __init__(self, name):
         self.name = name
 
+    def _check_container(self):
+        return docker.ps(filters={"name": self.name})
+
     def is_running(self):
-        container_found = self.call_container()
+        container_found = self._check_container()
 
         if container_found:
-            for container in self.call_container():
+            for container in self._check_container():
                 # Todo: logger
                 print(type(container))
                 print(container)
-                # print(f"Container is {container.state}")
                 if container.state.status == "running":
                     return True
                 else:
                     return False
         else:
-            print(f"Container '{self.name}' not found")
+            # Todo: logger
             return False
-
-    def call_container(self):
-        return docker.ps(all, filters={"name": self.name})
-
-
-if Container("busybox").is_running():
-    print("foo")
-else:
-    print("bar")
