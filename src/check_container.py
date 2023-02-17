1ef5902ff412c73832494385f003116743f7ad1f
Qaldak <qaldak@gmail.com>
2023-02-17 20:39:26 +0100

    check_container

--- src/check_container.py
+++ src/check_container.py
@@ -5,21 +5,14 @@ class Container:
     def __init__(self, name):
         self.name = name
 
-    def _check_container(self):
-        return docker.ps(filters={"name": self.name})
+    # Todo: Debug Logger
+    # def __get_container_info(self):
+    #     container_info = docker.ps(filters={"name": self.name, "status": "running"})
+    #     for container in container_info:
+    #         print(container.state)
 
     def is_running(self):
-        container_found = self._check_container()
-
-        if container_found:
-            for container in self._check_container():
-                # Todo: logger
-                print(type(container))
-                print(container)
-                if container.state.status == "running":
-                    return True
-                else:
-                    return False
+        if docker.ps(filters={"name": self.name, "status": "running"}):
+            return True
         else:
-            # Todo: logger
             return False
