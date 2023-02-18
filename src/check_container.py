c6d7e2f473a1088e8734f22cf11ce8ee4038cdd5
Roger <roger-kueng@bluewin.ch>
2023-02-18 13:50:58 +0100

    refactor: check_container .gitignore: add *.log

--- src/check_container.py
+++ src/check_container.py
@@ -1,18 +1,27 @@
-from python_on_whales import docker
+from python_on_whales import docker, DockerException
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Container:
     def __init__(self, name):
         self.name = name
 
-    # Todo: Debug Logger
-    # def __get_container_info(self):
-    #     container_info = docker.ps(filters={"name": self.name, "status": "running"})
-    #     for container in container_info:
-    #         print(container.state)
+    def __get_container_info(self):
+        for container in docker.ps(all=True, filters={"name": self.name}):
+            logger.debug(f"Container info for '{self.name}': {container.state}")
 
     def is_running(self):
-        if docker.ps(filters={"name": self.name, "status": "running"}):
-            return True
-        else:
-            return False
+        logger.debug("This is a debug thing")
+        try:
+            if docker.ps(filters={"name": self.name, "status": "running"}):
+                return True
+            else:
+                logger.warning(f"Docker Container '{self.name}' not found or is not running")
+                if logger.isEnabledFor(logging.DEBUG):
+                    self.__get_container_info()
+                return False
+
+        except DockerException as e:
+            logger.error(f"{DockerException.__name__}: Exit code {e.return_code}, {e.stderr.strip()}")
