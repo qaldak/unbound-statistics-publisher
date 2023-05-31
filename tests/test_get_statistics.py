d71b31c631026c85ba1ee004690e8dca55ca2a46
Qaldak <qaldak@gmail.com>
2023-05-31 21:45:55 +0200

    New GitHub action workflow for unit testing (#6)
    * Change test test_get_statistics.py
    * setup github workflow

--- tests/test_get_statistics.py
+++ tests/test_get_statistics.py
@@ -7,7 +7,7 @@ from src.get_statistics import Collector
 class TestCollectUnboundStatistics(TestCase):
 
     @patch("src.get_statistics.docker.execute",
-           return_value=(open("fixtures/unbound_stats.file").read()))
+           return_value=(open("tests/fixtures/unbound_stats.file").read()))
     def test_get_unbound_stats(self, unbound_stats):
         stats_json = Collector.get_statistics("foo")
         self.assertIsInstance(stats_json, str)
